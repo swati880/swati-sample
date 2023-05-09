fe04ba4358e0ad27eb4cbc508ea981902ecf1688
swati880 <132912845+swati880@users.noreply.github.com>
2023-05-09 17:29:50 +0000

    Added html page in Flask

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask , render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-  return "<p>what is ur name?</p> <form action = '/home/runner/swati-sample-1/home.py'> <input type = 'text'>&nbsp; <input type='submit' value='Submit'> </form>"
+  return render_template('home.html')
 
 
 if __name__ == "__main__":
--- home.py
+++ home.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-  return "what is your name"
+  return "<p>what is ur name?</p> <form action = '/home/runner/swati-sample-1/home.py'> <input type = 'text'>&nbsp; <input type='submit' value='Submit'> </form>"
 
 
 if __name__ == "__main__":
